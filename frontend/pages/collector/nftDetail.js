aa50c72c837018527f46806b8a5b60d19e81f89d
1ro8s <saki0220hiro@gmail.com>
2022-07-05 07:04:35 +0900

    不要なHeader削除

--- frontend/pages/collector/nftDetail.js
+++ frontend/pages/collector/nftDetail.js
@@ -3,17 +3,12 @@ import { Header } from "../../components/layout/header";
 import { Card } from "../../components/organisms/card";
 
 const MyNFTs = () => {
-  
-
   return (
     <>
-      <Header />
-      <div className="p-2">
+      <div className="flex flex-col items-center p-2">
         <Card />
       </div>
     </>
   );
 };
 export default MyNFTs;
-
-
