1b9809b3868532a812cbaf9f72cf850a629413de
1ro8s <saki0220hiro@gmail.com>
2022-07-06 04:04:53 +0900

    apiドメインを環境変数に変更

--- frontend/utils/utils.js
+++ frontend/utils/utils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const CONTRACT_ADDRESS= process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
 export const WEB3STORAGE_TOKEN = process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN
-const API_DOMAIN = "api-testnet.polygonscan.com";
+const POLYGONSCAN_API = process.env.NEXT_PUBLIC_POLYGONSCAN_API
 
 
 /**
@@ -12,7 +12,7 @@ const API_DOMAIN = "api-testnet.polygonscan.com";
 export const getABI = async () => {
   try {
     const res = await axios.get(
-      `https://${API_DOMAIN}/api?module=contract&action=getabi&address=${CONTRACT_ADDRESS}`
+      `https://${POLYGONSCAN_API}/api?module=contract&action=getabi&address=${CONTRACT_ADDRESS}`
     );
     const data = res.data;
     return data.result;
